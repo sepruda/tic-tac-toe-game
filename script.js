69994b11985895abaaaa7b454f355048ef05b113
Christian Jess Mark <christianjessmark@gmail.com>
2018-05-12 22:39:42 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const board = document.getElementById('board');
+// const squares = document.getElementsByTagName('td')
+const squares = $('td');
+
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+
+// console.log(one);
+
+$(squares).on('click', function(event){
+	
+	$(this).addClass('cross');
+});
